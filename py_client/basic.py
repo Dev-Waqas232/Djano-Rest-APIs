51e2bf20722080e0dbd095570efd7e0ebf97f4a6
Waqas Munir <devwaqas232@gmail.com>
2024-09-25 16:29:41 +0500

    Added New App for working with apis

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,5 +1,8 @@
 import requests
 
-endpoint = "https://httpbin.org"
+endpoint = "http://localhost:8000/"
 
 get_response = requests.get(endpoint)
+
+print(get_response.text)
+print(get_response.status_code)
